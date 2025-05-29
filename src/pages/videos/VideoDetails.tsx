35b98c90fa0f02ea26578a7ae2647931cd2707ea
Łukasz Kaliszewicz <lukasz.kaliszewicz01@gmail.com>
2025-05-29 12:28:32 +0200

    before installing strapi block react renderer

--- src/pages/videos/VideoDetails.tsx
+++ src/pages/videos/VideoDetails.tsx
@@ -18,23 +18,29 @@ const VideoDetails: React.FC = () => {
         const fetchVideo = async () => {
             const videoData = await getSingleVideo(documentId as string);
             setVideo(videoData);
+            console.log('Fetched video:', videoData);
         };
         fetchVideo();
     }, [documentId]);
 
-    const [videoInfo] = useState({
+    const [videoInfo, setVideoInfo] = useState({
         time: '02:30',
         director: 'Butter Pants',
         year: '2077',
         description: 'Intro XX edycji Węgiel Film Festival to krótka animacja otwierająca jubileuszową odsłonę festiwalu, którego tematem przewodnim była praca twórców filmowych ze scenariuszem.  Film został stworzony przez Gosię Gryko i Bruna Bednarskiego, studentów II roku produkcji w Szkole Filmowej im. Krzysztofa Kieślowskiego. Animacja wprowadza widza w atmosferę festiwalu, podkreślając znaczenie scenariusza jako fundamentu każdej produkcji filmowej. Poprzez dynamiczne obrazy i symboliczne przedstawienia, twórcy ukazują proces twórczy, jaki towarzyszy powstawaniu filmu  od pierwszych szkiców scenariusza po finalny obraz na ekranie.  Węgiel Film Festival to międzynarodowe wydarzenie organizowane przez studentów Szkoły Filmowej im. Krzysztofa Kieślowskiego w Katowicach, które od lat przyciąga młodych twórców z całego świata. Festiwal stanowi platformę do prezentacji krótkometrażowych filmów fabularnych i dokumentalnych oraz miejsce wymiany doświadczeń między początkującymi filmowcami a profesjonalistami z branży. ([Uniwersytet Śląski][3]) Intro XX edycji festiwalu nie tylko zapowiadało nadchodzące projekcje i wydarzenia, ale również oddawało hołd pracy scenarzystów, podkreślając ich kluczową rolę w procesie tworzenia filmu. Dzięki kreatywnej animacji i przemyślanej symbolice, filmik skutecznie wprowadzał widzów w tematykę festiwalu, zachęcając do refleksji nad znaczeniem scenariusza w kinematografii.([Facebook][2]) Całość stanowiła spójną i inspirującą zapowiedź wydarzenia, które celebruje pasję, kreatywność i zaangażowanie młodych twórców filmowych.',
     });
     
+    const testFn = () => {
+        console.log('Test function called');
+        console.log('Video Description:', video?.Description);
+    }
+
     return (
         <>
         <Header setIsMenuVisible={setIsMenuVisible} isMenuVisible={isMenuVisible}/>
         {isMenuVisible && <Menu />}
         <div className="videoDetails py-4 pt-20 pb-24 flex flex-col gap-16 items-center justify-start bg-black text-white min-h-screen">
-            
+            <button className='text-white text-2xl font-bold' onClick={testFn}>TestFn</button>
            <div className='videoBox w-[70%] p-0 m-0 aspect-video border' >   
                 {video ? (
                     <VideoSingleItem video={video} />
@@ -76,6 +82,7 @@ const VideoDetails: React.FC = () => {
                         </div>
                     </div>
                 </div>
+                {/* <p>{video.id ?? "pusto"}</p> */}
             </div>
         </div>
         
